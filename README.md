0d26e7e7dd0b7d3cbd9402d67d8a2290a152d93e
negineri <harusoin@gmail.com>
2020-01-30 22:51:28 +0000

    Add documents

---
+++ README.md
@@ -0,0 +1,5 @@
+# Backpacker
+## Author
+[@harusoin51](https://twitter.com/harusoin51)
+## License
+MIT License
--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"context"
 	"github.com/negineri/backpacker/backup"
+	"os"
 )
 
 func main() {
-	v := backup.New("unix", "/var/run/docker.sock", "v1.24", "/mnt/hdd1/backup/docker")
+	dest := os.Getenv("BACKPACKER_DEST")
+	if dest == "" {
+		dest = "/mnt/hdd1/backup/docker"
+	}
+	v := backup.New("unix", "/var/run/docker.sock", "v1.24", dest)
 	ctx := context.Background()
 	v.Monitor(ctx)
 }
